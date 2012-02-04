94d5d02e37ad92eee6c9f9692f970b5c92b73682
Kev <kevork.sepetci.125@my.csun.edu>
2012-02-04 11:56:58 -0800

    Initial Import with HTML5 boiler plate stuff.

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+	/**
+	 * This is a main template that a php script can include. The following variables should be set
+	 * before this file is included:
+	 *	$_title
+	 *	$_author
+	 *	$_description
+	 *	
+	 */
+?>
+><!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	  <title></title>
+	  <meta name="description" content="">
+	  <meta name="author" content="">
+
+	  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+	  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+	  <!-- CSS: implied media=all -->
+	  <link rel="stylesheet" href="css/style.css">
+	  <!-- end CSS-->
+
+
+	  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+	</head>
+
+	<body>
+
+	  <div id="container">
+		<header>
+			header
+		</header>
+		<div id="main" role="main">
+			<h1>We're Gonna Win!!!</h1>
+		</div>
+		<footer>
+			footer
+		</footer>
+	  </div> <!--! end of #container -->
+
+
+
+	  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+
+	  <!-- scripts concatenated and minified via ant build script-->
+	  <script defer src="js/plugins.js"></script>
+	  <script defer src="js/script.js"></script>
+	  <!-- end scripts-->
+
+
+
+	  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+		   chromium.org/developers/how-tos/chrome-frame-getting-started -->
+	  <!--[if lt IE 7 ]>
+		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+	  <![endif]-->
+	  
+	</body>
+</html>
